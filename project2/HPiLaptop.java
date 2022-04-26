86e86dbdbcf31af4e879a65c5d39534215004e90
Pujith Veeravelli <pujithvi@gmail.com>
2022-04-25 23:49:59 -0400

    Update HPiLaptop.java

---
+++ project2/HPiLaptop.java
@@ -0,0 +1,11 @@
+public class HPiLaptop extends Computer {
+    public HPiLaptop() {
+        name = "HPi Laptop";
+        memory = "multiple HPi memory chips";
+        CPU = "a powerful HPi cpu with many cores";
+        HDD = "multiple HPi hdds";
+        GPU = "a generic HPi gpu";
+
+        extras.add("a powerful HPi network card");
+    }
+}

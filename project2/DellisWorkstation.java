ad282d0fdb36242245791108fbd0ff725c7fb9a3
Pujith Veeravelli <pujithvi@gmail.com>
2022-04-25 23:58:27 -0400

    wrong class

---
+++ project2/DellisWorkstation.java
@@ -0,0 +1,12 @@
+public class DellisWorkstation extends Computer {
+    public DellisWorkstation() {
+        name = "Dellis Laptop";
+        memory = "Dellis memory";
+        CPU = "Dellis cpu";
+        HDD = "Dellis hdd";
+        GPU = "a powerful Dellis gpu";
+
+        extras.add("an audio card");
+        extras.add("a generic network card");
+    }
+}

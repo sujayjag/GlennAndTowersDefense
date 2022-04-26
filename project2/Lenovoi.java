fe7887f25d53c7f236ef9aaf2a5ccc95e2d9ab1a
Pujith Veeravelli <pujithvi@gmail.com>
2022-04-25 23:45:35 -0400

    Update Lenovoi.java

---
+++ project2/Lenovoi.java
@@ -0,0 +1,13 @@
+public class Lenovoi extends Pineapple {
+    Computer createComputer(String type) {
+        if (type.equals("workstation")) {
+            return new LenovoiWorkstation();
+        } else if (type.equals("laptop")) {
+            return new LenovoiLaptop();
+        } else if (type.equals("server")) {
+            return new LenovoiServer();
+        } else {
+            return null;
+        }
+    }
+}

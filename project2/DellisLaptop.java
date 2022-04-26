6864694f7d17f8e989dbcb80baa9da205f0e2260
Pujith Veeravelli <pujithvi@gmail.com>
2022-04-25 23:56:30 -0400

    Update DellisLaptop.java

---
+++ project2/DellisLaptop.java
@@ -0,0 +1,11 @@
+public class DellisLaptop extends Computer {
+    public DellisLaptop() {
+        name = "Dellis Laptop";
+        memory = "Dellis memory";
+        CPU = "Dellis cpu";
+        HDD = "Dellis hdd";
+        GPU = "a powerful Dellis gpu";
+
+        extras.add("a powerful HPi network card");
+    }
+}

26f8aa6e5faac6fe6f12c9acfdab19ff6dd98dfe
Pujith Veeravelli <pujithvi@gmail.com>
2022-04-25 23:51:20 -0400

    fix code in wrong class

---
+++ project2/HPiServer.java
@@ -0,0 +1,11 @@
+public class HPiServer extends Computer {
+    public HPiServer() {
+        name = "HPi Server";
+        memory = "multiple HPi memory chips";
+        CPU = "a powerful HPi cpu with many cores";
+        HDD = "multiple HPi hdds";
+        GPU = "a generic HPi gpu";
+
+        extras.add("a powerful HPi network card");
+    }
+}

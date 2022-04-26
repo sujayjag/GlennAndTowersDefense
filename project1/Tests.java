cd8a384b3f31f15a4398583381271429cff49539
Suraj <geddamsuraj@gmail.com>
2022-04-26 04:26:05 -0400

    changed to junits

--- project1/Tests.java
+++ project1/Tests.java
@@ -1,8 +1,7 @@
 package project1;
 
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.assertTrue;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
 
 public class Tests {
 

8d59c1437654a297fef5bee2692bb1f531d24dfe
Pujith Veeravelli <pujithvi@gmail.com>
2022-04-26 03:54:56 -0400

    add test file

---
+++ project3/project3new/Tests.java
@@ -0,0 +1,29 @@
+package project3new;
+
+import org.testng.annotations.Test;
+
+public class Tests {
+
+    @Test
+    public void Test1() {
+        //test if children of a node is equal to expected children
+    }
+
+    @Test
+    public void Test2() {
+        //test adding node to CEO child
+    }
+
+    @Test
+    public void Test3() {
+        //
+    }
+
+    @Test
+    public void Test4() {
+    }
+
+    @Test
+    public void Test5() {
+    }
+}

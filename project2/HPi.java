10a587c2700bf005ed56e314b9b84edb15df29f7
Pujith Veeravelli <pujithvi@gmail.com>
2022-04-25 23:45:59 -0400

    Update HPi.java

---
+++ project2/HPi.java
@@ -0,0 +1,13 @@
+public class Lenovoi extends Pineapple {
+    Computer createComputer(String type) {
+        if (type.equals("workstation")) {
+            return new HPiWorkstation();
+        } else if (type.equals("laptop")) {
+            return new HPiLaptop();
+        } else if (type.equals("server")) {
+            return new HPiWorkstation();
+        } else {
+            return null;
+        }
+    }
+}

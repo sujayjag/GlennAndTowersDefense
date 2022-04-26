36fa64281686b168fc2e9ff2c23d862939452e22
Pujith Veeravelli <pujithvi@gmail.com>
2022-04-26 01:02:15 -0400

    .

--- project1/IceCreamStore.java
+++ project1/IceCreamStore.java
@@ -7,12 +7,20 @@ public class IceCreamStore {
         iceCream1 = new Vanilla(iceCream1);
         iceCream1 = new HotFudge(iceCream1);
         iceCream1 = new Sprinkles(iceCream1);
-        System.out.println(iceCream1.getDescription() + " $" + iceCream1.getCost());
+        double cost = iceCream1.getCost();
+        cost*=100;
+        cost = Math.round(cost);
+        cost/=100;
+        System.out.println(iceCream1.getDescription() + " $" + cost);
 
         IceCream iceCream2 = new SingleScoop();
         iceCream2 = new Chocolate(iceCream2);
         iceCream2 = new Caramel(iceCream2);
         iceCream2 = new WhippedCream(iceCream2);
-        System.out.println(iceCream2.getDescription() + " $" + iceCream2.getCost());
+        cost = iceCream2.getCost();
+        cost*=100;
+        cost = Math.round(cost);
+        cost/=100;
+        System.out.println(iceCream2.getDescription() + " $" + cost);
     }
 }
